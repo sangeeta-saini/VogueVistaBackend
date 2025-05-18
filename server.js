2c8e24989be2f88b9b7f31cb8e6a022ac16cdb68
sangeetasaini <sangeetasainijpr02@gmail.com>
2025-05-18 14:36:38 +0530

    add / api

--- server.js
+++ server.js
@@ -29,6 +29,12 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use("/", () => {
+  return {
+    ok: true,
+  };
+});
+
 app.use("/brands", BrandRoutes);
 
 app.use("/user", UserRoutes);
