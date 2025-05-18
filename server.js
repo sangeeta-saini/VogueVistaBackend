160cf2d95f5d18a0b53e12441d8582da1b7ece7b
sangeetasaini <sangeetasainijpr02@gmail.com>
2025-05-18 14:45:54 +0530

    fix issue

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.use("/", () => {
-  return {
+app.use("/", (req, res) => {
+  res.json({
     ok: true,
-  };
+  });
 });
 
 app.use("/brands", BrandRoutes);
